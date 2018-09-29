e462be4149144dd279dbcba587252be8bb5f91c3
Revanth <revanth0212@gmail.com>
2018-09-29 13:19:11 -0500

    Setting up project structure.

--- src/App/Views/index.js
+++ src/App/Views/index.js
@@ -1,3 +1,3 @@
-import React from "react"
+import RegistrationPageView from './RegistrationPage'
 
-export default () => <div> App View </div>
+export default RegistrationPageView
--- src/App/index.js
+++ src/App/index.js
@@ -1,4 +1,3 @@
-import React from "react"
-import App from "./Views"
+import App from './Views'
 
-export default () => <App />
+export default App
